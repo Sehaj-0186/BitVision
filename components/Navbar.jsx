0864987041f651dfa05798ff67588790bea97155
varun kapoor <kapoorv046@gmail.com>
2025-01-22 17:06:29 +0530

    minor changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,16 +5,18 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between items-center h-[20vh] absolute top-0'>
-       <div className='flex items-center mx-16'>
-         <Image src={logo} alt='logo' width={60} height={60} />
-         <span className='font-cinzel font-semibold bg-gradient-to-r text-transparent bg-clip-text from-gray-500 via-gray-400 to-gray-500 text-xl'>NFTnexus</span>
-        </div> 
-       <div className='mx-16'>
-        <ConnectButton />
-       </div>
+    <div className="w-full flex justify-between items-center h-[20vh] absolute top-0">
+      <div className="flex items-center mx-16">
+        <Image src={logo} alt="logo" width={60} height={60} />
+        <span className="font-cinzel font-semibold bg-gradient-to-r text-transparent bg-clip-text from-gray-500 via-gray-400 to-gray-500 text-xl">
+          NFTnexus
+        </span>
+      </div>
+      <div className="mx-16">
+        <ConnectButton  />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
