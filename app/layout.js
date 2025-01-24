e1445fd2b09f9d18f23fc52678e5a61ea36848aa
varun kapoor <kapoorv046@gmail.com>
2025-01-24 18:36:58 +0530

    production changes

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Providers from "./providers";
 import "@rainbow-me/rainbowkit/styles.css";
 import { AppWrapper } from "../components/context";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,8 +26,11 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AppWrapper>
-          <Providers>{children}</Providers>
-          
+          <Providers>
+            {children}
+
+            <SpeedInsights />
+          </Providers>
         </AppWrapper>
       </body>
     </html>
